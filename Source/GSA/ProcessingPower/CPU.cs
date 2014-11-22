f22db83ff24eadd414a8cab31f26bc0e3e27e1f6
ThomasKerman <dorianstoll@googlemail.com>
2014-11-22 13:47:09 +0100

    Upate

---
+++ Source/GSA/ProcessingPower/CPU.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ProcessingPower
+{
+    public class CPU : PartModule
+    {
+        public int Processingpower;
+        public override void OnStart(StartState state)
+        {
+            base.OnStart(state);
+
+            Debug.Log("Plugin started correctly");
+        }
+
+        public override void OnLoad(ConfigNode node)
+        {
+            if (node.HasValue("MHZ"))
+            {
+                Processingpower = node.GetValue;
+            }
+        }
+
+        public void GenericresourceCreator()
+        {
+
+        }
+    }
+}
