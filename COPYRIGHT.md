2a27bcfb4a760a89c16a7f7174d21d82ad01a904
ThomasKerman <dorianstoll@googlemail.com>
2014-11-22 13:24:44 +0100

    Lift-Off

---
+++ COPYRIGHT.md
@@ -0,0 +1,10 @@
+GermanSpaceAlliance
+===============
+
+Copyrights:
+
+Quazar501:
+	Parts: QT-HomePC, QT-Keyboard+Mouse, QT-Processor1, QT-Processor3, QT-Processor3, QT-RAM2, QT-RAM3, QT-Screen
+	
+SpaceMaster: 
+	Plugins: ProcessingPower
---
+++ Source/GSA/ProcessingPower/ProcessorModule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ProcessingPower
+{
+    public class ProcessorModule : PartModule
+    {
+        public override void OnStart(StartState state)
+        {
+            base.OnStart(state);
+
+            Debug.Log("Plugin started correctly");
+        }
+    }
+}
